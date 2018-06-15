6f37fa942f4a05f7c16383c190583a9d6524a13c
Arturo Salas <artsaldel@hotmail.com>
2018-06-15 16:46:45 -0600

    Remove python 2.7.3 script and add yocto dependencies, including python 3.6

--- scripts/yocto.dependencies.sh
+++ scripts/yocto.dependencies.sh
@@ -1,1 +1,3 @@
-sudo apt-get install gawk wget git-core diffstat unzip texinfo gcc-multilib build-essential chrpath socat libsdl1.2-dev xterm
+sudo apt-get install gawk wget git-core diffstat unzip texinfo gcc-multilib \
+     build-essential chrpath socat cpio python python3 python3-pip python3-pexpect \
+     xz-utils debianutils iputils-ping libsdl1.2-dev xterm
